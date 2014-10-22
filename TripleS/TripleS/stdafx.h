d018577b60b5479848ae252c17b49f9c49d0458f
MyungKyun <ultraman232@naver.com>
2014-10-23 00:09:14 +0900

    TypeDef.h include add

--- TripleS/TripleS/stdafx.h
+++ TripleS/TripleS/stdafx.h
@@ -5,8 +5,10 @@
 #include <iostream>
 #include <stdio.h>
 #include <tchar.h>
+#include <assert.h>
 
 // TODO: 프로그램에 필요한 추가 헤더는 여기에서 참조합니다.
 #include "Macro.h"
 #include "FowardDeclaration.h"
 #include "WinSock.h"
+#include "TypeDef.h"
