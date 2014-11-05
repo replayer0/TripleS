1ff3124110c24c77f4acd95b4698cf5fadc9540f
replayer0 <replayer0@naver.com>
2014-11-05 16:07:23 +0900

    Change test logic

--- TripleS/TripleS/TripleS.cpp
+++ TripleS/TripleS/TripleS.cpp
@@ -5,22 +5,34 @@
 #endif // TRIPLE_S_DEBUG_MODE
 
 #include "TcpService.h"
+#include "TcpSocket.h"
+
+//test option
+#define num_socket 10
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-    //create
-    TripleS::TcpService server;
+    //create socket
+    TripleS::TcpSocket tcpsocket[num_socket];
+    Sleep(2000);
     
     //define desc
     TripleS::service_desc desc;
-    
+
+    //create
+    TripleS::TcpService server(desc);
+    Sleep(2000);
+
     //run
-    server.Start(desc);
+    server.Start();
+
+    //regist
+    for (int i = 0; i < num_socket; ++i)
+    {
+        server.RegistSocket(&tcpsocket[i]);
+    }
 
     Sleep(2000);
-    //wait
-    //server.Release();
-
 	return 0;
 }
 
