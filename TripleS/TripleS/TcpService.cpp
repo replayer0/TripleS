19a3ac4c4c5c17f1bea811f11bd0077a71ca3e14
MyungKyun <ultraman232@naver.com>
2014-11-23 12:25:30 +0900

    modified file and add include directory and library

--- TripleS/TripleS/TcpService.cpp
+++ TripleS/TripleS/TcpService.cpp
@@ -30,15 +30,15 @@ void TripleS::TcpService::Start()
     m_proactor->RunThread();
 }
 
-bool TripleS::TcpService::RegistFunction(PACKET_TYPE key, void(*func)(PacketPtr&))
-{
-    return RegistFunctor(key, new TripleS::FunctionFunctor<void(*)(PacketPtr&), PacketPtr&>(func));
-}
+// bool TripleS::TcpService::RegistFunction(PACKET_TYPE key, void(*func)(PacketPtr&))
+// {
+//     return RegistFunctor(key, new TripleS::FunctionFunctor<void(*)(PacketPtr&), PacketPtr&>(func));
+// }
 
-bool TripleS::TcpService::RegistFunctor(PACKET_TYPE key, TcpFunctor* base_functor)
-{
-    return m_functorAdapter.Regist(key, base_functor);
-}
+// bool TripleS::TcpService::RegistFunctor(PACKET_TYPE key, TcpFunctor* base_functor)
+// {
+//     return m_functorAdapter.Regist(key, base_functor);
+// }
 
 void TripleS::TcpService::_Release()
 {
